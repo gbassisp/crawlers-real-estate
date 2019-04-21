c167f5c2559a54c6f2d264f21198b78fdf899d3c
gbassisp <guilherme.bassi.sp@gmail.com>
2019-04-21 15:47:21 +0930

    new settings profile
    Created a new settings profile to override those on the common settings file

--- rec_scrapy/rec_scrapy/spiders/zapimoveis.py
+++ rec_scrapy/rec_scrapy/spiders/zapimoveis.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import unethical
 
 
 class ZapimoveisSpider(scrapy.Spider):
     name = 'zapimoveis'
     allowed_domains = ['zapimoveis.com.br']
     start_urls = ['http://zapimoveis.com.br/']
+    custom_settings = unethical.get_unethical_settings()	
 
     def parse(self, response):
         pass
---
+++ rec_scrapy/unethical.py
@@ -0,0 +1,14 @@
+#This file is intended to override crawling settings on a basic scrapy spider
+#The main goal is to keep the settings.py file simple and fitting most spider needs, so only the exceptions should import this
+
+#values to be overriden on the settings.py file are this:
+
+#ZapImoveis profile (unethical and "untraceable")
+def get_unethical_settings():
+	BOT_NAME = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+	ROBOTSTXT_OBEY = False
+	DOWNLOAD_DELAY = 40 #those bastards won't allow webscraping
+	COOKIES_ENABLED = False #I'm not sure this is necessary, but just in case
+	return {'BOT_NAME':BOT_NAME, 'ROBOTSTXT_OBEY':ROBOTSTXT_OBEY,'DOWNLOAD_DELAY':DOWNLOAD_DELAY,'COOKIES_ENABLED':COOKIES_ENABLED}
+
+	
