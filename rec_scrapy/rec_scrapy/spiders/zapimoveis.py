4938a9bc5da91d7af19eaa6af3a7ac21ccc5c75d
gbassisp <guilherme.bassi.sp@gmail.com>
2019-04-21 16:01:49 +0930

    unthical updates
    Added the name of the spider as a parameter to the get unethical settings on unethical module. The same method shall be called for every spider

--- rec_scrapy/rec_scrapy/spiders/zapimoveis.py
+++ rec_scrapy/rec_scrapy/spiders/zapimoveis.py
@@ -6,8 +6,10 @@ import unethical
 class ZapimoveisSpider(scrapy.Spider):
     name = 'zapimoveis'
     allowed_domains = ['zapimoveis.com.br']
-    start_urls = ['http://zapimoveis.com.br/']
-    custom_settings = unethical.get_unethical_settings()	
+    start_urls = ['http://zapimoveis.com.br/'] #this shall be updated to start from previous session
+    custom_settings = unethical.get_unethical_settings(name)	
+    #load list of urls to crawl
+
 
     def parse(self, response):
         pass
--- rec_scrapy/unethical.py
+++ rec_scrapy/unethical.py
@@ -3,12 +3,18 @@
 
 #values to be overriden on the settings.py file are this:
 
-#ZapImoveis profile (unethical and "untraceable")
-def get_unethical_settings():
-	BOT_NAME = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
-	ROBOTSTXT_OBEY = False
-	DOWNLOAD_DELAY = 40 #those bastards won't allow webscraping
-	COOKIES_ENABLED = False #I'm not sure this is necessary, but just in case
-	return {'BOT_NAME':BOT_NAME, 'ROBOTSTXT_OBEY':ROBOTSTXT_OBEY,'DOWNLOAD_DELAY':DOWNLOAD_DELAY,'COOKIES_ENABLED':COOKIES_ENABLED}
+
+def get_unethical_settings(name_of_the_spider=''):
+	#ZapImoveis profile (unethical and "untraceable")	
+	if name_of_the_spider == 'zapimoveis':
+		BOT_NAME = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+		ROBOTSTXT_OBEY = False
+		DOWNLOAD_DELAY = 40 #those bastards won't allow webscraping
+		COOKIES_ENABLED = False #I'm not sure this is necessary, but just in case
+		return {'BOT_NAME':BOT_NAME, 'ROBOTSTXT_OBEY':ROBOTSTXT_OBEY,'DOWNLOAD_DELAY':DOWNLOAD_DELAY,'COOKIES_ENABLED':COOKIES_ENABLED}
+	
+
+	#for any other profile, return an empty dictionary
+	return {}
 
 	
