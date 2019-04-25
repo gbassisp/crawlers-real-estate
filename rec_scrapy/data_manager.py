36a85cd2bc0350bd1c7e9e4216c028992b24d0f3
gbassisp <guilherme.bassi.sp@gmail.com>
2019-04-25 15:01:42 +0930

    Started MySQL phase
    Added new python file for loading and saving urls. Renamed unethical.py to custom_spider_settings.py to be more clear

---
+++ rec_scrapy/data_manager.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import pymysql
+
+
+class DataManager():
+	"""A custom MySQL managing module for all spiders"""
+	def __init__(self, arg):
+		super(DataManager, self).__init__()
+		self.arg = arg
+
+	def load_new_urls_from_domain(domain_name):
+		pass
+
+	def save_new_response():
+		pass
--- rec_scrapy/rec_scrapy/spiders/zapimoveis.py
+++ rec_scrapy/rec_scrapy/spiders/zapimoveis.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import unethical
+import custom_spider_settings as unethical
 
 
 class ZapimoveisSpider(scrapy.Spider):
     name = 'zapimoveis'
     allowed_domains = ['zapimoveis.com.br']
-    start_urls = ['http://zapimoveis.com.br/'] #this shall be updated to start from previous session
+    start_urls = ['https://www.zapimoveis.com.br/'] #this shall be updated to start from previous session
     custom_settings = unethical.get_unethical_settings(name)	
     #load list of urls to crawl
 
