3aaed1dee2688a473a77ca986aa6d85a7befe116
gbassisp <guilherme.bassi.sp@gmail.com>
2019-04-25 16:31:11 +0930

    Improved data_manager.py
    Defined first few methods on data_manager.py and created the credentials json file

--- rec_scrapy/data_manager.py
+++ rec_scrapy/data_manager.py
@@ -1,15 +1,30 @@
 # -*- coding: utf-8 -*-
 import pymysql
-
+import json
 
 class DataManager():
 	"""A custom MySQL managing module for all spiders"""
-	def __init__(self, arg):
-		super(DataManager, self).__init__()
-		self.arg = arg
+	file_name = 'credentials.json'
+	credentials = {}
 
-	def load_new_urls_from_domain(domain_name):
+	def load_credentials(self, file_name=file_name):
+		with open(file_name,'r') as file:
+			credentials = json.load(file)
+		return credentials
+
+	def connect_to_database(self, credentials=credentials):
+		c = credentials
+		return pymysql.connect(host=c['host'],user=c['user'],passwd=c['passwd'],port=c['port'], db=c['db'])
+
+	def load_new_urls_from_domain(self, domain_name):
 		pass
 
-	def save_new_response():
+	def save_new_response(self):
 		pass
+
+	def __init__(self, file_name=file_name):
+		#super(DataManager,	self).__init__()
+		arg = file_name
+		self.file_name = arg
+		self.credentials = self.load_credentials(arg)
+
