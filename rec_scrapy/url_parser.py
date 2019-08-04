22df35ff74b369e1d35199469b8813c6e55c6715
gbassisp <guilherme.bassi.sp@gmail.com>
2019-08-04 12:38:35 +0930

    Update url_parser.py
    Only returns url with the same domain

--- rec_scrapy/url_parser.py
+++ rec_scrapy/url_parser.py
@@ -1,6 +1,6 @@
 #This is a simple class to parse the html file and retrieve relevant URLs
 from html.parser import HTMLParser
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
 import re
 
 class URLParser(HTMLParser):
@@ -14,8 +14,16 @@ class URLParser(HTMLParser):
         else:
             self.output_list = url_input
         self.cur_url = cur_url
+        self.domain = urlparse(cur_url).netloc
+
     def handle_starttag(self, tag, attrs):
         if tag == 'a':
             #converts relative to absolute url before appending to the output list
             absolute_url = urljoin(self.cur_url, dict(attrs).get('href'))
-            self.output_list.append(absolute_url)
+            self.output_list.append(absolute_url) if self.check_domain(domain=self.domain,url=absolute_url) else None
+    
+    def check_domain(self, domain, url):
+        if domain in urlparse(url).netloc:
+            return True
+        else:
+            return False
