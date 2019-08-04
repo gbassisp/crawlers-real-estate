780c463e9d83bd4dcfe832105d75b455898e58e2
gbassisp <guilherme.bassi.sp@gmail.com>
2019-08-04 12:18:15 +0930

    Update zapimoveis.py
    Exception handling on parse

--- rec_scrapy/rec_scrapy/spiders/zapimoveis.py
+++ rec_scrapy/rec_scrapy/spiders/zapimoveis.py
@@ -24,17 +24,18 @@ class ZapimoveisSpider(scrapy.Spider):
 
 
     def parse(self, response):
-        response_id = self.dm.save_new_response(response.url, response.status) #update the response table with the link and HTTP code
         try:
+            response_id = self.dm.save_new_response(response.url, response.status) #update the response table with the link and HTTP code
             self.next_urls.remove(response.url)
-        except:
-            pass
-        parser_obj = url_parser.URLParser(response.url) #create a parser object for this response
-        parser_obj.feed(response.body.decode('utf-8')) #feed the HTML to the parser
-        new_links = list(set(parser_obj.output_list)) #this is smelly to me, but it works
-        self.dm.save_new_urls(new_links) #update the url table with the urls retrieved from this page
-        self.next_urls.update(self.dm.get_urls_to_crawl(list(self.next_urls))) #updates the crawling set
-        self.dm.save_new_file(response)
+            parser_obj = url_parser.URLParser(response.url) #create a parser object for this response
+            parser_obj.feed(response.body.decode('utf-8')) #feed the HTML to the parser
+            new_links = list(set(parser_obj.output_list)) #this is smelly to me, but it works
+            self.dm.save_new_urls(new_links) #update the url table with the urls retrieved from this page
+            self.next_urls.update(self.dm.get_urls_to_crawl(list(self.next_urls))) #updates the crawling set
+            self.dm.save_new_file(response)
+        except Exception as e:
+            print('Could not parse the response, catched ', e)
+        
 
         if self.next_urls is not None:
             #next_page = self.next_urls.pop()
