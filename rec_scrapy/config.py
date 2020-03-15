c2964002f6a547bbc0e7f9d20e3530d1d79b8408
gbassisp <guilherme.bassi.sp@gmail.com>
2020-03-15 14:40:04 +1030

    Create config file

---
+++ rec_scrapy/config.py
@@ -0,0 +1,19 @@
+#generic config
+db_type='mysql' #options are mysql or sqlite
+db_sqlite_file=None #'zapimoveis.sqlite'
+zzz_on_error = 15 # sleep time for the service to restart used at the main function
+zzz_ok = 45
+domains = ['zapimoveis.com.br']
+# default credentials, 
+# to be overriden by the credentials.py file
+db_credentials={
+    "host": "localhost", 
+    "user": "root", 
+    "passwd": "", 
+    "port": 3306, 
+    "db": "db-name"}
+
+try:
+    from credentials import *
+except:
+    print('No local credentials, using default')
