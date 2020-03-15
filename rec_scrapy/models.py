fceac7dee5044c16061546ead4855da5aeb79c96
gbassisp <guilherme.bassi.sp@gmail.com>
2020-03-15 19:58:20 +1030

    Update models: set nullable=false to necessary columns

--- rec_scrapy/models.py
+++ rec_scrapy/models.py
@@ -9,17 +9,17 @@ Base = declarative_base()
 class Domain(Base):
      '''Domain table contains the domains for the indexer'''
      __tablename__='domain'
-     id=Column(Integer, primary_key=True)
-     name=Column(String(255))
+     id=Column(Integer, primary_key=True, nullable=False)
+     name=Column(String(255), nullable=False)
      country=Column(String(3))
 
      pages = relationship('Page', back_populates='domain')
 class Page(Base):
      '''Page table contains the pages to be crawled'''
      __tablename__='page'
-     id=Column(Integer, primary_key=True)
-     domain_id=Column(Integer, ForeignKey('domain.id'))
-     full_url=Column(String(255))
+     id=Column(Integer, primary_key=True, nullable=False)
+     domain_id=Column(Integer, ForeignKey('domain.id'), nullable=False)
+     full_url=Column(String(255), nullable=False)
      crawl=Column(Boolean, default=True)
      create_date=Column(DateTime, default=dt.datetime.now())
 
@@ -28,10 +28,10 @@ class Page(Base):
 class Response(Base):
      '''Response table contains the response of each page when crawled'''
      __tablename__='response'
-     id=Column(Integer, primary_key=True)
-     page_id=Column(Integer, ForeignKey('page.id'))
-     response_code=Column(Integer)
-     visit_date=Column(DateTime, default=dt.datetime.now())
+     id=Column(Integer, primary_key=True, nullable=False)
+     page_id=Column(Integer, ForeignKey('page.id'), nullable=False)
+     response_code=Column(Integer, nullable=False)
+     visit_date=Column(DateTime, default=dt.datetime.now(), nullable=False)
      file_url=Column(String(255))
 
      page = relationship('Page', back_populates='responses')
