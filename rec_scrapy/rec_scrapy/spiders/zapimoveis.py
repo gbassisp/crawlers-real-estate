ffe099a8bd4d651c420f6c8d2bf722ec14398cf5
gbassisp <guilherme.bassi.sp@gmail.com>
2020-03-16 15:59:34 +1030

    Update spider to query the first few pages

--- rec_scrapy/rec_scrapy/spiders/zapimoveis.py
+++ rec_scrapy/rec_scrapy/spiders/zapimoveis.py
@@ -16,12 +16,13 @@ class ZapimoveisSpider(scrapy.Spider):
     def __init__(self):
         # Create data manager object
         self.dm = data_manager.DataManager()
-        self.start_urls = [self.dm.add_https_scheme(domain) for domain in self.allowed_domains]
+        self.start_urls = ['https://www.zapimoveis.com.br/lancamento/venda-apartamento-2-quartos-liberdade-centro-sao-paulo-sp-37m2-id-2476413410/'] #[self.dm.add_https_scheme(domain) for domain in self.allowed_domains]
         self.dm.set_save_file_settings(self.save_file, self.save_dir)
         self.dm.set_domains(self.allowed_domains, self.domain_country)
         # get urls to crawl
         self.next_urls = set()
         self.next_urls.update(self.dm.get_urls_to_crawl(list(self.next_urls)))
+        #self.start_urls = list(self.next_urls)
         
 
     
@@ -31,7 +32,9 @@ class ZapimoveisSpider(scrapy.Spider):
 
 
     def parse(self, response):
-        response_id, response_new = self.dm.save_new_response(response.url, response.status) #update the response table with the link and HTTP code
+        response_id, response_new = self.dm.save_new_response(response) #update the response table with the link and HTTP code
+        with open('daosijda.html', 'w') as f:
+            f.writelines(response.body.decode('utf-8'))
         try: #try parsing the page for new urls
             parser_obj = url_parser.URLParser(response.url) #create a parser object for this response
             parser_obj.feed(response.body.decode('utf-8')) #feed the HTML to the parser
@@ -40,7 +43,7 @@ class ZapimoveisSpider(scrapy.Spider):
         except Exception as e:
             print('Could not parse the response, catched ', e)
         self.next_urls = set(self.dm.get_urls_to_crawl(list(self.next_urls))) #updates the crawling set
-        self.dm.save_new_file(response)
+        #self.dm.save_new_file(response)
         
 
         if self.next_urls is not None:
